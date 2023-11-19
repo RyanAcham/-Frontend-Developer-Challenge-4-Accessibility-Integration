2a80cdff30bd69ee2ee2391794bfee057ee81a50
Ryan Acham <ryanacham@hotmail.com>
2023-11-19 16:07:23 -0500

    bug fixing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css"
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./comp/header";
+import TestPage from "./comp/test";
+import Footer from "./comp/footer";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<TestPage />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
