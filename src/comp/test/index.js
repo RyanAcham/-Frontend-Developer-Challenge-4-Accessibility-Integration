44759f1c40c57d5c88a08ee896a2382c901c7b7f
Ryan Acham <ryanacham@hotmail.com>
2023-11-19 17:14:14 -0500

    header and footer

---
+++ src/comp/test/index.js
@@ -0,0 +1,17 @@
+import { React } from "react";
+import "./style.css";
+import AccessibilityWidget from "../widget";
+
+
+function TestPage() {
+
+  return (
+    <div className="pageContainer text-center">
+        TEST PAGE 123
+        <br/>
+        <AccessibilityWidget />
+    </div>
+  );
+}
+
+export default TestPage;
